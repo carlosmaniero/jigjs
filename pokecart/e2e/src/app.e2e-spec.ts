4017068c6609b2ffc57ef8066ea87775ceb7b6bd
Carlos Maniero <carlosmaniero@gmail.com>
2020-04-21 01:42:04 -0300

    clean app component

--- pokecart/e2e/src/app.e2e-spec.ts
+++ pokecart/e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('displays welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('pokecart app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to PokéShop!');
   });
 
   afterEach(async () => {
--- pokecart/src/app/app.component.spec.ts
+++ pokecart/src/app/app.component.spec.ts
@@ -1,35 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import { render } from '@testing-library/angular'
+
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  it('renders title', async () => {
+    const component = await render(AppComponent, {
       imports: [
         RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'pokecart'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokecart');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('pokecart app is running!');
+      ]
+    });
+    expect(component.queryByText('Welcome to PokéShop!')).not.toBeNull();
   });
 });
