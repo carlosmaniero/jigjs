50a4cdd073cfd6432b72806cb86cd409bf4f0be4
Carlos Maniero <carlosmaniero@gmail.com>
2020-04-25 20:21:33 -0300

    replaces bash sintaxe to shell

--- run_all_test.sh
+++ run_all_test.sh
@@ -1,7 +1,11 @@
-function installAndTest() {
-    yarn --cwd $1 && yarn --cwd $1 test &&
+#!/bin/sh
+
+install_and_test()
+{
+  yarn --cwd "$1";
+  yarn --cwd "$1" test
 }
 
-installAndTest "frontends/cart" &&
-installAndTest "frontends/catalog" &&
-installAndTest "pokeshop-entry"
+install_and_test "frontends/cart" &&
+install_and_test "frontends/catalog" &&
+install_and_test "pokeshop-entry"
