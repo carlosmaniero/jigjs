1f6f921c657a8c31404049c758a4c196c12cd581
Carlos Maniero <carlosmaniero@gmail.com>
2020-04-25 20:10:37 -0300

    removes old orchestrator

--- pokeshop/src/app/front-end-fragment.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FrontEndFragmentDirective } from './front-end-fragment.directive';
-
-describe('FrontEndFragmentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FrontEndFragmentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
