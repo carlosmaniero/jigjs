41946ee74e4902c14545fc928c7ae2a6eb627c3e
carlosmaniero <carlosmaniero@gmail.com>
2020-05-17 17:20:50 -0300

    move all packages to be easier to export to npm registry

--- jig/jest.config.js
+++ jig/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: "node",
-    setupFilesAfterEnv: ['./src/testing/setup.jest.ts']
+    setupFilesAfterEnv: ['./testing/setup.jest.ts']
 };
