cb8bc87a61928865114f0a8a1fb1cbe44686f5cc
carlosmaniero <carlosmaniero@gmail.com>
2020-07-18 19:47:32 -0300

    adds jigcss based on template tags

--- jigcss/index.ts
+++ jigcss/index.ts
@@ -1,5 +1,3 @@
-import {JigWindow} from "jigjs/types";
-
 export type ElementStyle = {
   '@media': {
     [query: string]: ElementStyle
@@ -28,11 +26,63 @@ const hash = (x: string): number => {
   return phash(SEED, x);
 };
 
+const JIG_STYLE_ID = 'jig-style-element'
+
+export type JigCss = (template: TemplateStringsArray, ...values: unknown[]) => string;
+
+export const jigcss = (window: Document): JigCss => {
+  const getOrCreateStyleElement = () => {
+    return document.getElementById(JIG_STYLE_ID) || this.createStyleElement();
+  }
+
+  const createStyleElement = () => {
+    const element = document.createElement('style');
+    element.id = JIG_STYLE_ID;
+
+    document.head.appendChild(element);
+
+    return element;
+  }
+
+  const styleElement = getOrCreateStyleElement();
+
+  const appendToElement = (classes: string) => {
+    styleElement.appendChild(document.createTextNode(classes));
+  }
+
+  const hasClassHash = (className: string) => {
+    return styleElement.innerHTML.includes(className);
+  }
+
+  const formatStyle = (style: string, className: string) => {
+    const ampPlaceholder = '__jigcss-placeholder_amp_to_be_kept_jigcss-placeholder__';
+    return style
+      .replace(/\\&/g, ampPlaceholder)
+        .replace(/&/g, `.${className}`)
+        .replace(new RegExp(ampPlaceholder, 'g'), '&');
+  }
+
+  return (template: TemplateStringsArray, ...values: unknown[]): string => {
+    const style = String.raw(template, ...values);
+
+    const styleHash = hash(style);
+    const className = `jc-${styleHash}`;
+
+    if (hasClassHash(`.${className}`)) {
+      return className;
+    }
+
+    appendToElement(formatStyle(style, className));
+
+    return className;
+  };
+}
+
 export class Css {
   private static JIG_STYLE_ID = 'jig-style-element';
   private styleElement: HTMLElement;
 
-  constructor(private readonly window: JigWindow) {
+  constructor(private readonly window: Window) {
     this.styleElement = this.getOrCreateStyleElement();
   }
 
