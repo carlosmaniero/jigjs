f204e63fb792994e0faa676ecf2d917b36195c66
carlosmaniero <carlosmaniero@gmail.com>
2020-07-19 02:35:56 -0300

    removes attribute based css

--- jigcss/index.ts
+++ jigcss/index.ts
@@ -39,7 +39,7 @@ export const css = (template: TemplateStringsArray, ...values: unknown[]): JigCs
 
 const jigcss = (document: Document) => {
   const getOrCreateStyleElement = () => {
-    return document.getElementById(JIG_STYLE_ID) || this.createStyleElement();
+    return document.getElementById(JIG_STYLE_ID) || createStyleElement();
   }
 
   const createStyleElement = () => {
@@ -83,90 +83,4 @@ const jigcss = (document: Document) => {
 
     return className;
   };
-}
-
-export class Css {
-  private static JIG_STYLE_ID = 'jig-style-element';
-  private styleElement: HTMLElement;
-
-  constructor(private readonly window: Window) {
-    this.styleElement = this.getOrCreateStyleElement();
-  }
-
-  style(elementStyle: ElementStyle): string {
-
-    this.validateElementStyle(elementStyle);
-
-    const className = this.createClassHash(elementStyle);
-
-    if (!this.hasClassHash(className)) {
-      this.appendToElement(this.createStylesFor(elementStyle, className));
-    }
-    return className;
-  }
-
-  private createClassHash(style: ElementStyle) {
-    return "jig-" + hash(JSON.stringify(style));
-  }
-
-  private hasClassHash(className: string) {
-    return this.styleElement.innerHTML.includes('.' + className + '{');
-  }
-
-  private createStylesFor(elementStyle: ElementStyle | Record<string, string>, className: string) {
-    return Object.keys(elementStyle).map((elementStyleKey) => {
-      if (elementStyleKey === '@media') {
-        const media = elementStyle['@media'] as Record<string, Record<string, Record<string, string>>>;
-
-        return Object.keys(media).map((query) => {
-          const classes = media[query];
-
-          return `@media ${query} {${this.createStylesFor(classes, className)}}`;
-        }).join('');
-      }
-
-      const selector = this.createSelector(className, elementStyleKey);
-      return `${selector}{${this.toClassBody(elementStyle[elementStyleKey])}}`
-    }).join('');
-  }
-
-  private createSelector(className: string, elementStyleKey: string) {
-    if (!elementStyleKey.startsWith('&')) {
-      throw new Error(`The selector must starts with "&". Found: "${elementStyleKey}".`);
-    }
-    return elementStyleKey.replace(/&/g, '.' + className);
-  }
-
-  private validateElementStyle(elementStyle: ElementStyle) {
-    if (elementStyle['@media'] && !(typeof elementStyle['@media'] === 'object')) {
-      throw new Error(`The @media selector must be an object. Found: "${typeof elementStyle['@media']}".`);
-    }
-  }
-
-  private getOrCreateStyleElement() {
-    return this.window.document.getElementById(Css.JIG_STYLE_ID) || this.createStyleElement();
-  }
-
-  private createStyleElement() {
-    const element = this.window.document.createElement('style');
-    element.id = Css.JIG_STYLE_ID;
-
-    this.window.document.head.appendChild(element);
-
-    return element;
-  }
-
-  private appendToElement(classes: string) {
-    this.styleElement.appendChild(this.window.document.createTextNode(classes));
-  }
-
-  private toClassBody(elementStyleElement: Record<string, string>) {
-    return Object.keys(elementStyleElement)
-      .map((property) => `${this.toKebabCase(property)}: ${elementStyleElement[property]}`)
-      .join(';');
-  }
-
-  private toKebabCase(property: string) {
-    return property.replace(/([a-z])([A-Z])/g, '$1-$2').replace(/\s+/g, '-').toLowerCase();
-  }
 }
