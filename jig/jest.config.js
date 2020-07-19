75eb920d1725a921d9b0348483315c3bcf8b1612
carlosmaniero <carlosmaniero@gmail.com>
2020-07-18 23:25:17 -0300

    ignores files that there is no intention to test

--- jig/jest.config.js
+++ jig/jest.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "testPathIgnorePatterns": [
         "<rootDir>/cli"
     ],
+    collectCoverageFrom: [
+        "**/*.ts",
+        "!<rootDir>/cli/**/*",
+        "!<rootDir>/framework/patform/*", // Typo to deprecate
+        "!<rootDir>/deprecation.ts", // Just a bunch of deprecation warnings (not tested when it is a module change)
+        "!<rootDir>/framework/server/server.ts" // There is no intention to start the real express application, otherwise its configuration is well tested.
+    ],
     globals: {
         'ts-jest': {
             tsconfig: './tsconfig.json'
